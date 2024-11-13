0fb67f1389e753bac6a3dbc08e6a1809cb054cf9
PinSpuMS <pin.ch@spulive.net>
2024-11-13 10:31:00 +0700

    Delete source code

--- src/data/logins.jsx
+++
@@ -1,9 +0,0 @@
-const logins = [{ user: 'user', pass: 'pass', token: 'token', roll: 'manager' }]
-
-export const apiPostTokenLogin = (url, data) => {
-  const resultLogin = logins.find(
-    (lg) => lg.user === data.user && lg.pass === data.pass
-  )
-  if (resultLogin === undefined) return ''
-  else return resultLogin.token
-}
